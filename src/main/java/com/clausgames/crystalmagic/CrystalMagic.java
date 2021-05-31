e0d204f389371a6843313dce6d0b855124d5be1d
TheClaus <eric.s.clausing@gmail.com>
2021-05-31 12:22:29 -0600

    CrystalMagic project initialization

---
+++ src/main/java/com/clausgames/crystalmagic/CrystalMagic.java
@@ -0,0 +1,10 @@
+package com.clausgames.crystalmagic;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CrystalMagic implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello and welcome to CrystalMagic!");
+    }
+}
